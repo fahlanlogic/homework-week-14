c589551cd7361859572ba2c8dd5e35c6ea10ea05
fahlanlogic <alanfahdi@gmail.com>
2024-06-24 21:18:12 +0700

    add button sign up

--- src/app/sign-in/page.jsx
+++ src/app/sign-in/page.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import toast, { Toaster } from "react-hot-toast";
@@ -70,6 +71,14 @@ export default function Register() {
           placeholder="********"
           className="px-2 py-1 rounded-md bg-black placeholder:text-white border border-slate-500 focus:outline-none focus:border-black placeholder:opacity-50"
         />
+        <p>
+          Don't have account?{" "}
+          <Link
+            href="/sign-up"
+            className="text-yellow-500">
+            Sign Up
+          </Link>
+        </p>
         <button
           type="submit"
           className="w-full bg-gradient-to-b from-yellow-500 to-yellow-600 py-2 rounded-md font-bold text-black hover:opacity-75 duration-300">
